635a3041650af3b83a06c0e618604f24706fa71a
digitaloracle <hazzzy@gmail.com>
2017-09-04 15:46:03 +0300

    another meaningless commit

---
+++ code_exfil_test.js
@@ -0,0 +1,50 @@
+alert("paypal")
+alert("funkyword123")
+alert("123123")
+rw-r--r--   928 azalmanovitch  8 May 14:35 ├── test_certificate.cert
+.rw-r--r--@ 8.8k azalmanovitch 14 Jul  2016 ├── testing.xlsx
+.rw-r--r--@ 220k azalmanovitch 22 Sep  2016 ├── testing_policy.xlsx
+.rw-r--r--@ 832k azalmanovitch 14 Feb 10:43 ├── time_related_vontu_stats.html
+.rw-r--r--@  62k azalmanovitch  5 Sep  2016 ├── tnuot_2016-09-05.pdf
+.rw-r--r--@ 529k azalmanovitch  7 Sep  2016 ├── tofes_prime_lease.pdf
+.rw-r--r--@  22M azalmanovitch  4 Jan 12:52 ├── top%2d1m.csv
+.rw-rw-rw-@ 838k azalmanovitch 17 Aug  8:41 ├── top%20SMB%20merchants%20acquired%20Q2%20%28all%29.txt
+drwxr-xr-x@    - azalmanovitch  5 Mar 16:56 ├── transactions%5ffor%5fsio
+.rw-r--r--@ 2.8M azalmanovitch  9 Feb 14:30 │  ├── ATO.csv
+.rw-r--r--@  35M azalmanovitch  9 Feb 14:34 │  ├── GOOD.csv
+.rw-r--r--@ 2.4M azalmanovitch  9 Feb 14:32 │  └── SF.csv
+.rw-rw-rw-@ 3.8M azalmanovitch 15 Aug 17:03 ├── travel.pptx
+.rw-r--r--@  11k azalmanovitch  3 Jul 11:50 ├── vid-correction
+.rw-r--r--@  337 azalmanovitch  2 Aug  2016 ├── vim-scala_README.txt
+.rw-r--r--  117k azalmanovitch 18 Jan 11:21 ├── violator.html
+.rw-r--r--@ 184k azalmanovitch 18 Jan 11:22 ├── violator_959268.html
+.rw-r--r--@ 1.8M azalmanovitch 14 Dec  2016 ├── virtualenv-15.1.0-py2.py3-none-any.whl
+.rw-r--r--@ 152k azalmanovitch 20 Apr 16:10 ├── vontu_500.csv
+.rw-r--r--  1.6M azalmanovitch 23 May 15:26 ├── vontu_sqlite3.exe
+.rw-r--r--@ 353k azalmanovitch 25 Apr 14:43 ├── vpn_log.txt
+.rw-r--r--   23k azalmanovitch 25 Apr 14:51 ├── vpn_logs.7z
+.rw-r--r--@ 8.8k azalmanovitch  9 Mar 13:35 ├── vtfeeds.py
+.rw-r--r--@ 109k azalmanovitch 15 Dec  2016 ├── w2%5f2014.pdf
+.rw-r--r--@  30k azalmanovitch 23 Jan  9:02 ├── w2.pdf
+.rw-rw-rw-@ 2.7M azalmanovitch 20 Jul 14:02 ├── webview%2d2.log
+.rw-r--r--@ 942k azalmanovitch 25 Aug  2016 ├── weekly%5ftickler%5fdevops%5ftickets.xls
+.rw-r--r--@ 151k azalmanovitch  9 Mar 11:58 ├── wensun.csv
+.rw-------  6.9k azalmanovitch  9 Mar 11:59 ├── wensun.csv.gpg
+.rwxr-xr-x   147 azalmanovitch  4 Jul  2016 ├── wildfire_report.sh
+.rw-r--r--@  50k azalmanovitch  4 Jul  2016 ├── wildfire_report_5748895acdc6e5a34ed2f6af.xml
+.rw-r--r--@ 5.5k azalmanovitch 20 Apr 16:33 ├── www.paypalcredit.com%5fcert%5fswap.rtf
+.rw-------  4.6k azalmanovitch 20 Apr 16:35 ├── www.paypalcredit.com%5fcert%5fswap.rtf.gpg
+.rw-r--r--@ 1.3M azalmanovitch 14 Dec  2016 ├── xwb.zip
+.rw-r--r--@ 534k azalmanovitch 29 Jun  2016 ├── yPaypal%20International%20notes%20annual%20accounts%2031.12.2015%20v2.pdf
+.rw-r--r--@ 6.3M azalmanovitch 10 Jan 10:35 ├── yandex_microsoft_facebook_2_4.csv
+.rw-r--r--@ 5.8M azalmanovitch 10 Jan 10:38 ├── yandex_microsoft_facebook_5_6.csv
+.rw-r--r--@  162 azalmanovitch  3 Jan 14:11 ├── ~$-PayPal - China - Shanghai SAFE 2017 Outbound Quota and   Re-registration CHN-865725-v1-SHIDMS (002).docx
+.rw-r--r--@  171 azalmanovitch 21 Dec  2016 ├── ~$2013.xlsx
+.rw-r--r--@  171 azalmanovitch 13 Apr 17:09 ├── ~$Admin Tools Role Descriptions.xlsx
+.rw-------@  171 azalmanovitch  4 Jul  2016 ├── ~$C%3a%5cUsers%5cgangadi%5cDesktop%5cGuru%5cDesktop%5cAudit%20Tool%206.1%5cAudit%20Tool%206.1%5cMore%20Scope%20For%20MVC.xlsx
+.rw-r--r--@  171 azalmanovitch  2 Jan 12:56 ├── ~$Copy%20of%20Copy%20of%20650%20211000.xlsx
+.rw-------@  171 azalmanovitch 31 May  2016 ├── ~$DLP-Actions-Timeline.xlsx
+.rw-------@  171 azalmanovitch 21 Jun  2016 ├── ~$Microsoft%5fExcel%5fWorksheet1.xlsx
+.rw-------@  171 azalmanovitch 14 Aug  2016 ├── ~$Microsoft%5fExcel%5fWorksheet10.xlsx
+.rw-r--r--@  171 azalmanovitch  2 Feb  9:52 ├── ~$September%202016.xlsx
+
